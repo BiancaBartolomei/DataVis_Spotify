7b9b004346fc2b8407f2cf623ec87dd10dcaade6
Luis Malta <luiso.malta@gmail.com>
2018-11-10 15:34:56 -0200

    [FIX]HTML Header Bug

--- dashboard.py
+++ dashboard.py
@@ -10,9 +10,11 @@ from datetime import date as dt
 
 
 
-engine = create_engine('')
+engine = create_engine('postgres://luismalta:Lo3355199731@localhost:5432/spotify_db')
 
-df_features_musicas = pd.read_sql_query('select * from spotify_db.features_musicas',con=engine)
+df_features_track = pd.read_sql_query('select * from spotify_db.features_track',con=engine)
+df_features_artist= pd.read_sql_query('select * from spotify_db.features_artist',con=engine)
+df_features_playlist = pd.read_sql_query('select * from spotify_db.features_playlist',con=engine)
 
 
 
@@ -22,12 +24,14 @@ external_scripts = ['assets/material.js']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, external_scripts=external_scripts)
 server = app.server
 
+app.config.suppress_callback_exceptions = True
+
 app.title = "SPOTIFY POPULARITY DATABASE"
 
 def update_dropdown_features_music():
     opt_music= []
     musics = []
-    for music in df_features_musicas['track_name']:
+    for music in df_features_track['track_name']:
 
         if music not in musics:
             a = {'label':music, 'value':music}
@@ -74,7 +78,7 @@ app.layout = html.Div(children=[
     # --------- Main --------------
     html.Main([
         html.Div(id='page-main',className='page-content'),
-    ], className='mdl-layout__content'),
+    ]),
 
 ])
 
@@ -82,51 +86,16 @@ app.layout = html.Div(children=[
 # PAGE FEATURES
 
 page_features = html.Div([
-
-    # ------------ Header --------------------
-    html.Div(children=[
-        html.Header([
-            html.Div([
-                html.Span(children='Spotify Dashboard', className='mdl-layout-title'),
-
-                html.Div([
-
-                ],className='mdl-layout-spacer'),
-
-                html.Nav([
-
-                ], className='mdl-navigation mdl-layout--large-screen-only')
-
-            ], className='mdl-layout__header-row')
-        ], className='mdl-layout__header'),
-
-        html.Div([
-            html.Span(children='Menu', className='mdl-layout-title'),
-
-            html.Nav([
-                dcc.Link('Top 10', href='/top-10', className='mdl-navigation__link'),
-                dcc.Link('Análise de grupos', href='page-grupos', className='mdl-navigation__link'),
-                dcc.Link('Análise de Features', href='page-features', className='mdl-navigation__link'),
-                dcc.Link('Análise de popularidade', href='/page-popularidade', className='mdl-navigation__link'),
-            ], className="mdl-navigation")
-        ], className='mdl-layout__drawer')
-    ], className='mdl-layout mdl-js-layout mdl-layout--fixed-header'),
-
-
-    # --------- Main --------------
-    html.Main([
-        html.Div([
-            html.Div([
                 html.Div([
                     dcc.Graph(
                         id='radar-feature-musica',
                         figure={
                             'data': [go.Scatterpolar(
-                                r=[df_features_musicas['track_liveness'][4], df_features_musicas['track_speechness'][4],
-                                   df_features_musicas['track_valence'][4], df_features_musicas['track_energy'][4],
-                                   df_features_musicas['track_acousticness'][4],
-                                   df_features_musicas['track_instrumentalness'][4],
-                                   df_features_musicas['track_dancebility'][4]],
+                                r=[df_features_track['track_liveness'][4], df_features_track['track_speechness'][4],
+                                   df_features_track['track_valence'][4], df_features_track['track_energy'][4],
+                                   df_features_track['track_acousticness'][4],
+                                   df_features_track['track_instrumentalness'][4],
+                                   df_features_track['track_dancebility'][4]],
                                 theta=['Liveness', 'Speechness', 'Valence', 'Energy', 'Acousticness',
                                        'Instrumentalness', 'Dancebility'],
                                 fill='toself'
@@ -143,7 +112,7 @@ page_features = html.Div([
 
                         }
                     ),
-                ], className='mdl-cell mdl-cell--4-col'),
+                ], className='mdl-cell mdl-cell--12-col'),
 
                 html.Div([
                     dcc.Dropdown(
@@ -152,54 +121,14 @@ page_features = html.Div([
                         multi=False,
                         value=""
                     ),
-                ], className='mdl-cell mdl-cell--4-col'),
+                ], className='mdl-cell mdl-cell--6-col'),
 
             ], className= 'mdl-grid'),
-
-
-        ], className='page-content')
-    ], className='mdl-layout__content'),
-], id='page-feature')
 # ----------------------------------------------------------------------------------------------------------------------
 
 # PAGE TOP 10
 
-page_top_10 = html.Div(children=[
-
-    # ------------ Header --------------------
-    html.Div(children=[
-        html.Header([
-            html.Div([
-                html.Span(children='Spotify Dashboard', className='mdl-layout-title'),
-
-                html.Div([
-
-                ],className='mdl-layout-spacer'),
-
-                html.Nav([
-
-                ], className='mdl-navigation mdl-layout--large-screen-only')
-
-            ], className='mdl-layout__header-row')
-        ], className='mdl-layout__header'),
-
-        html.Div([
-            html.Span(children='Menu', className='mdl-layout-title'),
-
-            html.Nav([
-                dcc.Link('Top 10', href='/top-10', className='mdl-navigation__link'),
-                dcc.Link('Análise de grupos', href='page-grupos', className='mdl-navigation__link'),
-                dcc.Link('Análise de Features', href='page-features', className='mdl-navigation__link'),
-                dcc.Link('Análise de popularidade', href='/page-popularidade', className='mdl-navigation__link'),
-            ], className="mdl-navigation")
-        ], className='mdl-layout__drawer')
-    ], className='mdl-layout mdl-js-layout mdl-layout--fixed-header'),
-
-
-    # --------- Main --------------
-    html.Main([
-        html.Div([
-            html.Div([
+page_top_10 =  html.Div([
                 dcc.Graph(
                     id='musicas_populares',
                     figure={
@@ -211,63 +140,8 @@ page_top_10 = html.Div(children=[
                         }
                     }
                 ),
-            ], className='mdl-card'),
-        ], className='page-content')
-    ], className='mdl-layout__content'),
-])
+            ], className='mdl-grid')
 # ----------------------------------------------------------------------------------------------------------------------
-
-# Callback Dropdown features musicas
-# @app.callback(
-#     dash.dependencies.Output('radar-feature-musica','figure'),
-#     [dash.dependencies.Input('dropdown-feature-music','value')])
-# def update_figure(track_input):
-#     filtro = pd.DataFrame(columns=['track_name','artist_name','track_popularity','data_popularidade','artist_genre'])
-#     temp = df
-#
-#
-#     if track_input:
-#         temp = temp.loc[df['track_name'] == track_input]
-#
-#         filtro = temp
-#     else:
-#         filtro = pd.DataFrame(columns=['track_name','artist_name','track_popularity','data_popularidade','artist_genre'])
-#
-#
-#
-#     data_update = []
-#
-#     traces.append(
-#         go.Scatterpolar(
-#             r=[df_features_musicas['track_liveness'][1], df_features_musicas['track_speechness'][1],
-#                df_features_musicas['track_valence'][1], df_features_musicas['track_energy'][1],
-#                df_features_musicas['track_acousticness'][1], df_features_musicas['track_instrumentalness'][1],
-#                df_features_musicas['track_dancebility'][1], ],
-#             theta=['Liveness', 'Speechness', 'Valence', 'Energy', 'Acousticness', 'Instrumentalness', 'Dancebility'],
-#             fill='toself'
-#         )
-#     )
-#
-#     layout_update = []
-#
-#     layout_update.append(
-#         go.Layout(
-#             polar=dict(
-#                 radialaxis=dict(
-#                     visible=True,
-#                     range=[0, 1]
-#                 )
-#             ),
-#             showlegend=True
-#         )
-#     )
-#
-#
-#     return {
-#         'data': data_update,
-#         'layout': layout_update,
-#     }
-#
 
 
 # Callback Paginas
@@ -284,7 +158,6 @@ def display_page(pathname):
         return page_top_10
     else:
         return page_top_10
-    # You could also return a 404 "URL not found" page here
 
 
 if __name__ == '__main__':
