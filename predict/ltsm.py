216387521dc96e21f64e16a9f0222ea1371e6460
Bianca Bartolomei <biancarbartolomei@gmail.com>
2018-11-27 19:33:05 -0200

    [ADD]PREDICT

--- predict/ltsm.py
+++ predict/ltsm.py
@@ -1,270 +1,91 @@
-# import pandas as pd
-# from datetime import datetime as dt
-# from sqlalchemy import create_engine
-#
-# import numpy as np
+import pandas as pd
+from datetime import datetime as dt
+from sqlalchemy import create_engine
+import numpy as np
 from keras import optimizers
-#
-# import matplotlib.pyplot as plt
-# import math
-# from keras.models import Sequential
-# from keras.layers import Dense
-# from keras.layers import LSTM
-# from sklearn.preprocessing import MinMaxScaler
-# from sklearn.metrics import mean_squared_error
-
-
-from pandas import DataFrame
-from pandas import Series
-from pandas import concat
-from pandas import read_csv
-from pandas import datetime
-from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+import math
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
-from math import sqrt
-from matplotlib import pyplot
-import numpy
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
 
 
-def parser(x):
-	return datetime.strptime(x, '“%Y-%m-%d”')
+np.random.seed(7)
+
+engine = create_engine('postgres://biancabartolomei:19972015@localhost:5432/spotify')
+
+dataframe = pd.read_sql_query("select * from spotify_db.track_popularity where track_id='4as4XEOR03oGm1STUKl6pa'", con=engine, parse_dates=['data_popularidade'])
+dataframe.drop('track_id', axis=1, inplace=True)
+dataframe.drop('data_popularidade', axis=1, inplace=True)
+
+dataset = dataframe.values
+dataset = dataset.astype('float32')
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+
+print(len(train), len(test))
 
 
-
-# frame a sequence as a supervised learning problem
-def timeseries_to_supervised(data, lag=1):
-    df = DataFrame(data)
-    columns = [df.shift(i) for i in range(1, lag + 1)]
-    columns.append(df)
-    df = concat(columns, axis=1)
-    df.fillna(0, inplace=True)
-    return df
-
-
-# create a differenced series
-def difference(dataset, interval=1):
-    diff = list()
-    for i in range(interval, len(dataset)):
-        value = dataset[i] - dataset[i - interval]
-        diff.append(value)
-    return Series(diff)
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, look_back=1):
+    dataX, dataY = [], []
+    for i in range(len(dataset)-look_back-1):
+        a = dataset[i:(i+look_back), 0]
+        dataX.append(a)
+        dataY.append(dataset[i + look_back, 0])
+    return np.array(dataX), np.array(dataY)
 
 
-# invert differenced value
-def inverse_difference(history, yhat, interval=1):
-    return yhat + history[-interval]
+# reshape into X=t and Y=t+1
+look_back = 1
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+
+# reshape input to be [samples, time steps, features]
+trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+
+# create and fit the LSTM network
+model = Sequential()
+model.add(LSTM(4, input_shape=(1, look_back)))
+model.add(Dense(1))
+sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=1, nesterov=True)
+model.compile(loss='mean_squared_error', optimizer=sgd)
+# model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(trainX, trainY, epochs=1000, batch_size=1, verbose=2)
+
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+# invert predictions
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
+testY = scaler.inverse_transform([testY])
+# calculate root mean squared error
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+print('Test Score: %.2f RMSE' % (testScore))
 
 
-# scale train and test data to [-1, 1]
-def scale(train, test):
-    # fit scaler
-    scaler = MinMaxScaler(feature_range=(-1, 1))
-    scaler = scaler.fit(train)
-    # transform train
-    train = train.reshape(train.shape[0], train.shape[1])
-    train_scaled = scaler.transform(train)
-    # transform test
-    test = test.reshape(test.shape[0], test.shape[1])
-    test_scaled = scaler.transform(test)
-    return scaler, train_scaled, test_scaled
-
-
-# inverse scaling for a forecasted value
-def invert_scale(scaler, X, value):
-    new_row = [x for x in X] + [value]
-    array = numpy.array(new_row)
-    array = array.reshape(1, len(array))
-    inverted = scaler.inverse_transform(array)
-    return inverted[0, -1]
-
-
-# fit an LSTM network to training data
-def fit_lstm(train, batch_size, nb_epoch, neurons):
-    X, y = train[:, 0:-1], train[:, -1]
-    X = X.reshape(X.shape[0], 1, X.shape[1])
-    model = Sequential()
-    model.add(LSTM(neurons, batch_input_shape=(batch_size, X.shape[1], X.shape[2]), stateful=True))
-    model.add(Dense(1))
-    sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=1, nesterov=True)
-    model.compile(loss='mean_squared_error', optimizer=sgd)
-    for i in range(nb_epoch):
-        model.fit(X, y, epochs=1000, batch_size=batch_size, verbose=2)
-        model.reset_states()
-    return model
-
-
-# make a one-step forecast
-def forecast_lstm(model, batch_size, X):
-    X = X.reshape(1, 1, len(X))
-    yhat = model.predict(X, batch_size=batch_size)
-    return yhat[0, 0]
-
-
-# load dataset
-series = read_csv('teste1.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
-
-# transform data to be stationary
-raw_values = series.values
-diff_values = difference(raw_values, 1)
-
-# transform data to be supervised learning
-supervised = timeseries_to_supervised(diff_values, 1)
-supervised_values = supervised.values
-
-# split data into train and test-sets
-train, test = supervised_values[0:-6], supervised_values[-6:]
-
-# transform the scale of the data
-scaler, train_scaled, test_scaled = scale(train, test)
-
-# fit the model
-lstm_model = fit_lstm(train_scaled, 1, 3000, 4)
-# forecast the entire training dataset to build up state for forecasting
-train_reshaped = train_scaled[:, 0].reshape(len(train_scaled), 1, 1)
-lstm_model.predict(train_reshaped, batch_size=1)
-
-
-
-repeats = 1
-error_scores = list()
-for r in range(repeats):
-    # fit the model
-    lstm_model = fit_lstm(train_scaled, 1, 3000, 4)
-    # forecast the entire training dataset to build up state for forecasting
-    train_reshaped = train_scaled[:, 0].reshape(len(train_scaled), 1, 1)
-    lstm_model.predict(train_reshaped, batch_size=1)
-    # walk-forward validation on the test data
-    predictions = list()
-    for i in range(len(test_scaled)):
-        # make one-step forecast
-        X, y = test_scaled[i, 0:-1], test_scaled[i, -1]
-        yhat = forecast_lstm(lstm_model, 1, X)
-        # invert scaling
-        yhat = invert_scale(scaler, X, yhat)
-        # invert differencing
-        yhat = inverse_difference(raw_values, yhat, len(test_scaled) + 1 - i)
-        # store forecast
-        predictions.append(yhat)
-
-    # report performance
-    rmse = sqrt(mean_squared_error(raw_values[-6:], predictions))
-    print('%d) Test RMSE: %.3f' % (r + 1, rmse))
-    error_scores.append(rmse)
-
-# pre = forecast_lstm(lstm_model, 1, [1.0])
-# print(pre)
-# line plot of observed vs predicted
-pyplot.plot(raw_values[-6:])
-pyplot.plot(predictions)
-pyplot.show()
-
-results = DataFrame()
-results['rmse'] = error_scores
-print(results.describe())
-results.boxplot()
-pyplot.show()
-
-# def parser(x):
-# 	return dt.strptime(x, '“%Y-%m-%d”')
+# shift train predictions for plotting
+trainPredictPlot = np.empty_like(dataset)
+trainPredictPlot[:, :] = np.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+# shift test predictions for plotting
+testPredictPlot = np.empty_like(dataset)
+testPredictPlot[:, :] = np.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+# plot baseline and predictions
+plt.plot(scaler.inverse_transform(dataset))
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.show()
 #
-# series = pd.read_csv('teste1.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
-# # summarize first few rows
-# print(series.head())
-# # line plot
-# series.plot()
-# plt.show()
-
-
-
-
-
-
-# np.random.seed(7)
-# engine = create_engine('postgres://biancabartolomei:19972015@localhost:5432/spotify')
-# #
-# dataframe = pd.read_sql_query("select * from spotify_db.track_popularity where track_id='4as4XEOR03oGm1STUKl6pa'", con=engine, parse_dates=['data_popularidade'])
-# dataframe.drop('track_id', axis=1, inplace=True)
-# dataframe.drop('data_popularidade', axis=1, inplace=True)
-# print(dataframe.head(10))
-#
-# dataset = dataframe.values
-# dataset = dataset.astype('float32')
-#
-# # np.random.seed(7)
-# #
-# # dataset = dataframe['track_popularity'].values.astype('float32')
-#
-#
-# # load the dataset
-# # dataframe = pd.read_csv('teste1.csv', usecols=[1], engine='python')
-# # dataset = dataframe.values
-# # dataset = dataset.astype('float32')
-# scaler = MinMaxScaler(feature_range=(0, 1))
-# dataset = scaler.fit_transform(dataset)
-#
-# train_size = int(len(dataset) * 0.67)
-# test_size = len(dataset) - train_size
-# train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-# print(len(train), len(test))
-#
-#
-# # convert an array of values into a dataset matrix
-# def create_dataset(dataset, look_back=1):
-#     dataX, dataY = [], []
-#     for i in range(len(dataset)-look_back-1):
-#         a = dataset[i:(i+look_back), 0]
-#         dataX.append(a)
-#         dataY.append(dataset[i + look_back, 0])
-#     return np.array(dataX), np.array(dataY)
-#
-#
-# # reshape into X=t and Y=t+1
-# look_back = 1
-# trainX, trainY = create_dataset(train, look_back)
-# testX, testY = create_dataset(test, look_back)
-#
-# # reshape input to be [samples, time steps, features]
-# trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
-# testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
-#
-# # create and fit the LSTM network
-# model = Sequential()
-# model.add(LSTM(4, input_shape=(1, look_back)))
-# model.add(Dense(1))
-# sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=1, nesterov=True)
-# model.compile(loss='mean_squared_error', optimizer=sgd)
-# # model.compile(loss='mean_squared_error', optimizer='adam')
-# model.fit(trainX, trainY, epochs=1000, batch_size=1, verbose=2)
-#
-# trainPredict = model.predict(trainX)
-# testPredict = model.predict(testX)
-# # invert predictions
-# trainPredict = scaler.inverse_transform(trainPredict)
-# trainY = scaler.inverse_transform([trainY])
-# testPredict = scaler.inverse_transform(testPredict)
-# testY = scaler.inverse_transform([testY])
-# # calculate root mean squared error
-# trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
-# print('Train Score: %.2f RMSE' % (trainScore))
-# testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
-# print('Test Score: %.2f RMSE' % (testScore))
-#
-#
-# # shift train predictions for plotting
-# trainPredictPlot = np.empty_like(dataset)
-# trainPredictPlot[:, :] = np.nan
-# trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
-# # shift test predictions for plotting
-# testPredictPlot = np.empty_like(dataset)
-# testPredictPlot[:, :] = np.nan
-# testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
-# # plot baseline and predictions
-# plt.plot(scaler.inverse_transform(dataset))
-# plt.plot(trainPredictPlot)
-# plt.plot(testPredictPlot)
-# plt.show()
-# #
-# # model.predict(X, verbose=0)
+# model.predict(X, verbose=0)
